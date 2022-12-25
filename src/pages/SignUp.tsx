b32f5e2d668a7fef6e9dc350ab230ae8ad24cdd9
Anderson Fernandes <anderson.fernandes96@outlook.com>
2022-12-25 14:01:44 -0300

    Refactoring register page

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,20 +1,14 @@
-import { useCallback, useEffect, useRef, useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
+import { useCallback, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 import { Form } from "@unform/web";
 import { FormHandles } from "@unform/core";
-import getValidationErrors from "../utils/getValidateErrors";
 import * as Yup from "yup";
-import { Link, useNavigate } from "react-router-dom";
-import { ImArrowLeft } from "react-icons/im";
 import { FcGoogle } from "react-icons/fc";
-import { AiOutlineArrowRight } from "react-icons/ai";
-import { FiSave } from "react-icons/fi";
+
+import getValidationErrors from "../utils/getValidateErrors";
 import { Input } from "../components/Form/Input";
-import Banner from "../assets/image_onboarding.png";
-import BusinessImg from "../assets/business_image.png";
-import PurchaseImg from "../assets/purchase_image.png";
-import { Button } from "../components/Button";
 import api from "../services/api";
 
 interface SignUpFormData {
@@ -29,20 +23,11 @@ export const SignUp: React.FC = () => {
     const navigate = useNavigate();
 
     const [selectedType, setSelectedType] = useState("");
-    const [type, setType] = useState(false);
 
-    const handleSelectedType = useCallback(
-        (value: string) => {
-            setSelectedType(String(value));
-        },
-        [setSelectedType]
-    );
+    const handleSelectObjectiveOption = useCallback((value: string) => {
+        setSelectedType(String(value));
 
-    const handleNextStep = useCallback(() => {
-        setType(true);
-
-        console.log(selectedType);
-    }, [setType, selectedType]);
+    }, [setSelectedType]);
 
     const handleSubmit = useCallback(
         async (data: SignUpFormData) => {
@@ -65,9 +50,12 @@ export const SignUp: React.FC = () => {
 
                 console.log(data);
 
-                if (selectedType === "Venda") {
+                if (selectedType === "venda") {
                     data.isEntrepreneur = true;
                 }
+
+                console.log(data.isEntrepreneur);
+                console.log(data);
 
                 await api.post("/users", data);
 
@@ -90,127 +78,102 @@ export const SignUp: React.FC = () => {
     );
 
     return (
-        <>
+        <div className="bg-gray-100">
             <ToastContainer />
-            {type ? (
-                <div className="flex flex-row mobile:flex-col justify-between mobile:justify-start min-h-screen mobile:w-screen">
-                    <div className="flex w-1/2 h-screen mobile:h-48 mobile:w-full">
-                        <img
-                            src={Banner}
-                            alt="Banner"
-                            className="w-screen object-cover border-r-4 border-gray-300 mobile:border-none"
-                        />
-                    </div>
-                    <div className="flex flex-col items-center mobile:items-start justify-center w-1/2 bg-blue-900 mobile:bg-white mobile:w-screen">
-                        <Link className="mobile:hidden" to="/">
-                            <div className="flex flex-row items-center gap-2">
-                                <ImArrowLeft color="#FFFFFF" />
-                                <span className="font-montserrat text-white tex-mobile">
-                                    Voltar
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-xl">
+                            Registre-se
+                        </h1>
+                        <Form
+                            className="space-y-4 md:space-y-6"
+                            ref={formRef}
+                            onSubmit={handleSubmit}
+                        >
+
+                            <div>
+                                <span className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                                    Qual o seu objetivo?
                                 </span>
-                            </div>
-                        </Link>
-                        <div className="flex flex-col bg-white h-[34.188rem] w-[28.5rem] rounded mt-4 mobile:mt-0">
-                            <div className="flex items-center p-8 h-[4.125rem] w-full mobile:w-screen rounded-t mobile:rounded-none bg-indigo-600">
-                                <span className="font-inter font-medium uppercase text-white text-2xl">
-                                    Cadastrar
-                                </span>
-                            </div>
+                                <div className="flex items-center gap-4 mb-4">
+                                    <div>
+                                        <input
+                                            id="compra"
+                                            type="radio"
+                                            value=""
+                                            name="type"
+                                            onChange={() => handleSelectObjectiveOption('compra')}
+                                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+                                        />
+                                        <label
+                                            htmlFor="compra"
+                                            className="ml-2 text-sm font-medium text-gray-800"
+                                        >
+                                            Compra
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <input
+                                            id="venda"
+                                            type="radio"
+                                            value=""
+                                            name="type"
+                                            onChange={() => handleSelectObjectiveOption('venda')}
+                                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+                                        />
+                                        <label
+                                            htmlFor="venda"
+                                            className="ml-2 text-sm font-medium text-gray-800"
+                                        >
+                                            Venda
+                                        </label>
+                                    </div>
 
-                            <div className="flex flex-col mt-8">
-                                <Form
-                                    className="flex flex-col items-center gap-2"
-                                    ref={formRef}
-                                    onSubmit={handleSubmit}
-                                >
-                                    <Input
-                                        name="name"
-                                        label="Nome"
-                                        placeholder="Digite o seu nome"
-                                    />
-                                    <Input
-                                        name="email"
-                                        label="E-mail"
-                                        placeholder="Digite o seu e-mail"
-                                    />
-                                    <Input
-                                        name="password"
-                                        label="Senha"
-                                        type="password"
-                                        placeholder="Digite a sua senha"
-                                    />
-
-                                    <button
-                                        type="submit"
-                                        className="flex items-center justify-center mt-6 gap-3 bg-indigo-400 w-40 h-12 rounded hover:brightness-90 transition-opacity"
-                                    >
-                                        <FiSave size={24} color="#FFFFFF" />
-                                        <span className="font-inter text-lg text-white uppercase">
-                                            Salvar
-                                        </span>
-                                    </button>
-                                </Form>
-
-                                <div className="flex flex-row items-center gap-[0.938rem] px-[3.875rem] mt-8 cursor-pointer hover:brightness-90">
-                                    <FcGoogle size={24} />
-                                    <span className="font-montserrat font-semibold text-sm">
-                                        Cadastrar com o Google
-                                    </span>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            ) : (
-                <div className="flex flex-col items-center pt-[3.25rem] min-h-screen min-w-screen bg-blue-900">
-                    <h1 className="font-inter font-medium mobile:text-center text-4xl mobile:text-2xl text-white">
-                        Qual o seu objetivo com a plataforma?
-                    </h1>
-                    <div className="flex flex-row mobile:flex-col items-center justify-center gap-[8.25rem] mobile:gap-[4.125rem] mt-[3.625rem]">
-                        <button
-                            className="flex flex-col items-center justify-center gap-6 mobile:gap-3"
-                            onClick={() => {
-                                handleSelectedType("Compra");
-                            }}
-                        >
-                            <img
-                                src={PurchaseImg}
-                                alt=""
-                                className={`h-[18.75rem] w-[18.75rem] mobile:h-[12.5rem] mobile:w-[12.5rem] rounded-full border-4 border-gray-600  hover:border-blue-400 duration-200 hover:cursor-pointer ${selectedType == "Compra" ? "border-blue-400" : ""
-                                    }`}
-                            />
-                            <span className="font-inter text-3xl text-white uppercase">
-                                Compra
-                            </span>
-                        </button>
 
-                        <button
-                            className="flex flex-col items-center justify-center gap-6"
-                            onClick={() => {
-                                handleSelectedType("Venda");
-                            }}
-                        >
-                            <img
-                                src={BusinessImg}
-                                alt=""
-                                className={`h-[18.75rem] w-[18.75rem] mobile:h-[12.5rem] mobile:w-[12.5rem] rounded-full border-4 border-gray-600 hover:border-blue-400 duration-200 hover:cursor-pointer ${selectedType == "Venda" ? "border-blue-400" : ""
-                                    }`}
-                            />
-                            <span className="font-inter text-3xl text-white uppercase">
-                                Venda
-                            </span>
-                        </button>
-                    </div>
-                    <div className="flex flex-row items-center mt-12 text-white">
-                        <Button onClick={handleNextStep} disabled={!selectedType}>
-                            <span className="font-inter font-semibold text-xl uppercase">
-                                Avançar
-                            </span>
-                            <AiOutlineArrowRight size={24} />
-                        </Button>
+                            <div>
+                                <Input
+                                    name="name"
+                                    label="Nome"
+                                    placeholder="Digite o seu nome"
+                                />
+                            </div>
+                            <div>
+                                <Input
+                                    name="email"
+                                    label="E-mail"
+                                    type="email"
+                                    placeholder="Digite um e-mail"
+                                />
+                            </div>
+                            <div>
+                                <Input
+                                    name="password"
+                                    label="Senha"
+                                    type="password"
+                                    placeholder="Digite uma senha"
+                                />
+                            </div>
+                            <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                                Cadastrar
+                            </button>
+                            <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+                                <p className="text-center font-semibold text-gray-400 text-sm mx-4 mb-0">OU</p>
+                            </div>
+                            <button
+                                className="px-7 py-3 text-black font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
+                                type="button"
+
+                            >
+                                <FcGoogle size={24} />
+                                <span className="ml-2">Registrar com Google</span>
+                            </button>
+                        </Form>
                     </div>
                 </div>
-            )}
-        </>
+            </div>
+        </div>
     );
 };
