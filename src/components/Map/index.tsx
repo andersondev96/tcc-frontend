8f82033cf7b4025e29359aae008966730b5a69a5
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-01-28 00:53:42 -0300

    Navbar adjustments

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,94 +1,93 @@
 import { useEffect, useRef, useState, useCallback } from "react";
 import {
-  MapContainer,
-  Marker,
-  TileLayer,
-  useMapEvent,
-  useMap,
+    MapContainer,
+    Marker,
+    TileLayer,
+    useMapEvent,
+    useMap,
 } from "react-leaflet";
 import { Popup } from "./Popup";
 import CoffeeImg from "../../assets/coffee.png";
 
 interface IMapProps {
-  lat: number;
-  lng: number;
+    lat: number;
+    lng: number;
 }
 
 export const Map: React.FC<IMapProps> = ({ lat, lng }) => {
-  const animateRef = useRef(false);
-  const [latitude, setLatitude] = useState(0);
-  const [longitude, setLongitude] = useState(0);
-  let coords = {
-    lat: 0,
-    lng: 0,
-  };
+    const animateRef = useRef(false);
+    const [latitude, setLatitude] = useState(0);
+    const [longitude, setLongitude] = useState(0);
+    let coords = {
+        lat: 0,
+        lng: 0,
+    };
 
-  function setLocalization() {
-    if (lat === 0 && lng === 0) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setLatitude(position.coords.latitude);
-        setLongitude(position.coords.longitude);
-      });
-    } else {
-      setLatitude(lat);
-      setLongitude(lng);
+    function setLocalization() {
+        if (lat === 0 && lng === 0) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                setLatitude(position.coords.latitude);
+                setLongitude(position.coords.longitude);
+            });
+        } else {
+            setLatitude(lat);
+            setLongitude(lng);
+        }
     }
-  }
 
-  const center = [51.505, -0.09];
-  const zoom = 13;
+    const center = [51.505, -0.09];
+    const zoom = 13;
 
-  const handleSetView = useCallback(() => {
-    const map = useMap();
-    map.setView([51.505, -0.09], zoom);
-  }, []);
+    const handleSetView = useCallback(() => {
+        const map = useMap();
+        map.setView([51.505, -0.09], zoom);
+    }, []);
 
-  function SetViewOnClick({ animateRef }: any) {
-    const map = useMapEvent("click", (e) => {
-      map.setView([latitude, longitude], map.getZoom(), {
-        animate: animateRef.current || false,
-      });
-    });
+    function SetViewOnClick({ animateRef }: any) {
+        const map = useMapEvent("click", (e) => {
+            map.setView([latitude, longitude], map.getZoom(), {
+                animate: animateRef.current || false,
+            });
+        });
 
-    return null;
-  }
+        return null;
+    }
 
-  useEffect(() => {
-    setLocalization();
-  }, [setLocalization]);
+    useEffect(() => {
+        setLocalization();
+    }, [setLocalization]);
 
-  return latitude && longitude ? (
-    <div>
-      <MapContainer
-        className="w-screen h-screen absolute flex mobile:min-w-min"
-        center={[latitude, longitude]}
-        zoom={13}
-        scrollWheelZoom={true}
-      >
-        <TileLayer
-          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${
-            import.meta.env.VITE_ACCESS_TOKEN_MAP_BOX
-          }`}
-        />
-        <Marker position={[latitude, longitude]}>
-          <Popup
-            image={CoffeeImg}
-            name="Singhtglass Coffee"
-            category="Cafeteria"
-          />
-        </Marker>
+    return latitude && longitude ? (
+        <div>
+            <MapContainer
+                className="w-screen h-screen absolute z-10 flex mobile:min-w-min"
+                center={[latitude, longitude]}
+                zoom={13}
+                scrollWheelZoom={true}
+            >
+                <TileLayer
+                    url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${import.meta.env.VITE_ACCESS_TOKEN_MAP_BOX
+                        }`}
+                />
+                <Marker position={[latitude, longitude]}>
+                    <Popup
+                        image={CoffeeImg}
+                        name="Singhtglass Coffee"
+                        category="Cafeteria"
+                    />
+                </Marker>
 
-        <Marker position={[-19.6020179, -43.2177258]}>
-          <Popup
-            image={CoffeeImg}
-            name="Singhtglass Coffee"
-            category="Cafeteria"
-          />
-        </Marker>
-        <SetViewOnClick animateRef={animateRef} />
-      </MapContainer>
-    </div>
-  ) : (
-    <div></div>
-  );
+                <Marker position={[-19.6020179, -43.2177258]}>
+                    <Popup
+                        image={CoffeeImg}
+                        name="Singhtglass Coffee"
+                        category="Cafeteria"
+                    />
+                </Marker>
+                <SetViewOnClick animateRef={animateRef} />
+            </MapContainer>
+        </div>
+    ) : (
+        <div></div>
+    );
 };
