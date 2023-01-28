e5f71e7e712bdeba46130f7900a92bad9c90d07d
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-01-28 14:01:31 -0300

    Adjustments in services page

--- src/pages/client/Service/index.tsx
+++ src/pages/client/Service/index.tsx
@@ -1,68 +1,64 @@
-import { AiOutlineSearch } from "react-icons/ai";
-import { Header } from "../../../components/Header";
-import { BusinessHeader } from "../components/BusinessHeader";
 import { Card } from "../components/Card";
+import { Search } from "../../../components/Search";
+import { NavBar } from "../../../components/NavBar/NavBar";
 
 import Coffee1 from "../../../assets/coffee-img1.jpg";
-import { Search } from "../../../components/Search";
 
 export const Service: React.FC = () => {
-  
 
-  return (
-    <div className="flex flex-col">
-      <Header />
-      <BusinessHeader />
-      <div className="px-[6.25rem] py-[2.625rem] mobile:px-[1.125rem] mobile:py-[1.75rem]">
-        <h1 className="font-montserrat font-medium text-2xl">Serviços oferecidos</h1>
-        <div className="mt-[2rem] mobile:mt-[1rem]">
-          <Search />
-        </div>
-        <div className="mt-[3.65rem] flex flex-col font-montserrat font-semibold text-xl">
-          <span>Em destaque</span>
-          <div className="mt-3 grid grid-cols-3 gap-4 mobile:grid-cols-1">
-            <Card image={Coffee1} product="Café simples" stars={5} price="R$ 2,00" />
-            <Card image={Coffee1} product="Café simples" stars={3} price="R$ 2,00" />
-            <Card image={Coffee1} product="Café simples" stars={2} price="R$ 2,00" />
-          </div>
-        </div>
+    return (
+        <div className="flex flex-col">
+            <NavBar pageCurrent="servicos" />
+            <div className="px-4 py-7 sm:px-24 sm:py-10">
+                <h1 className="font-montserrat font-medium text-2xl">Serviços oferecidos</h1>
+                <div className="mt-8 sm:mt-8">
+                    <Search />
+                </div>
+                <div className="mt-11 flex flex-col font-montserrat font-semibold text-xl">
+                    <span>Em destaque</span>
+                    <div className="mt-3 grid grid-cols-1 sm:grid-cols-3 gap-4">
+                        <Card image={Coffee1} product="Café simples" stars={5} price="R$ 2,00" />
+                        <Card image={Coffee1} product="Café simples" stars={3} price="R$ 2,00" />
+                        <Card image={Coffee1} product="Café simples" stars={2} price="R$ 2,00" />
+                    </div>
+                </div>
 
-        <div className="mt-[3.65rem] flex flex-col font-montserrat font-semibold text-xl">
-          <span>Bebidas quentes</span>
-          <div className="mt-3 grid grid-cols-3 gap-4 mobile:grid-cols-1">
-            <Card image={Coffee1} product="Café simples" stars={5} price="R$ 2,00" />
-            <Card image={Coffee1} product="Café simples" stars={5} price="R$ 2,00" />
-            <Card image={Coffee1} product="Café simples" stars={4} price="R$ 2,00" />
-          </div>
-        </div>
+                <div className="mt-8 flex flex-col font-montserrat font-semibold text-xl">
+                    <span>Bebidas quentes</span>
+                    <div className="mt-3 grid grid-cols-1 sm:grid-cols-3 gap-4">
+                        <Card image={Coffee1} product="Café simples" stars={5} price="R$ 2,00" />
+                        <Card image={Coffee1} product="Café simples" stars={5} price="R$ 2,00" />
+                        <Card image={Coffee1} product="Café simples" stars={4} price="R$ 2,00" />
+                    </div>
+                </div>
 
-        <div className="mt-[3.65rem] flex flex-col font-montserrat font-semibold text-xl">
-          <span>Bebidas geladas</span>
-          <div className="mt-3 grid grid-cols-3 gap-4 mobile:grid-cols-1">
-            <Card image={Coffee1} product="Café simples" stars={1} price="R$ 2,00" />
-            <Card image={Coffee1} product="Café simples" stars={5} price="R$ 2,00" />
-            <Card image={Coffee1} product="Café simples" stars={5} price="R$ 2,00" />
-          </div>
-        </div>
+                <div className="mt-8 flex flex-col font-montserrat font-semibold text-xl">
+                    <span>Bebidas geladas</span>
+                    <div className="mt-3 grid grid-cols-1 sm:grid-cols-3 gap-4">
+                        <Card image={Coffee1} product="Café simples" stars={1} price="R$ 2,00" />
+                        <Card image={Coffee1} product="Café simples" stars={5} price="R$ 2,00" />
+                        <Card image={Coffee1} product="Café simples" stars={5} price="R$ 2,00" />
+                    </div>
+                </div>
 
-        <div className="mt-[3.65rem] flex flex-col font-montserrat font-semibold text-xl">
-          <span>Chás</span>
-          <div className="mt-3 grid grid-cols-3 gap-4 mobile:grid-cols-1">
-            <Card image={Coffee1} product="Café simples" stars={5} price="R$ 2,00" />
-            <Card image={Coffee1} product="Café simples" stars={2} price="R$ 2,00" />
-            <Card image={Coffee1} product="Café simples" stars={5} price="R$ 2,00" />
-          </div>
-        </div>
+                <div className="mt-8 flex flex-col font-montserrat font-semibold text-xl">
+                    <span>Chás</span>
+                    <div className="mt-3 grid grid-cols-1 sm:grid-cols-3 gap-4">
+                        <Card image={Coffee1} product="Café simples" stars={5} price="R$ 2,00" />
+                        <Card image={Coffee1} product="Café simples" stars={2} price="R$ 2,00" />
+                        <Card image={Coffee1} product="Café simples" stars={5} price="R$ 2,00" />
+                    </div>
+                </div>
 
-        <div className="mt-[3.65rem] flex flex-col font-montserrat font-semibold text-xl">
-          <span>Doces</span>
-          <div className="mt-3 grid grid-cols-3 gap-4 mobile:grid-cols-1">
-            <Card image={Coffee1} product="Café simples" stars={4} price="R$ 2,00" />
-            <Card image={Coffee1} product="Café simples" stars={4} price="R$ 2,00" />
-            <Card image={Coffee1} product="Café simples" stars={2} price="R$ 2,00" />
-          </div>
+                <div className="mt-8 flex flex-col font-montserrat font-semibold text-xl">
+                    <span>Doces</span>
+                    <div className="mt-3 grid grid-cols-1 sm:grid-cols-3 gap-4">
+                        <Card image={Coffee1} product="Café simples" stars={4} price="R$ 2,00" />
+                        <Card image={Coffee1} product="Café simples" stars={4} price="R$ 2,00" />
+                        <Card image={Coffee1} product="Café simples" stars={2} price="R$ 2,00" />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/pages/client/components/Card.tsx
+++ src/pages/client/components/Card.tsx
@@ -7,70 +7,70 @@ import { AiFillHeart } from "react-icons/ai";
 import { ModalService } from "./ModalService";
 
 interface CardProps {
-  image: string;
-  product: string;
-  stars: number;
-  price: string;
+    image: string;
+    product: string;
+    stars: number;
+    price: string;
 }
 
 export const Card: React.FC<CardProps> = ({ image, product, stars, price }) => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  function openModal() {
-    setModalIsOpen(true);
-  }
+    function openModal() {
+        setModalIsOpen(true);
+    }
 
-  function closeModal() {
-    setModalIsOpen(false);
-  }
+    function closeModal() {
+        setModalIsOpen(false);
+    }
 
-  const [displayHeart, setDisplayHeart] = useState(false);
-  const [like, setLike] = useState(false);
+    const [displayHeart, setDisplayHeart] = useState(false);
+    const [like, setLike] = useState(false);
 
-  return (
-    <div
-      className="flex flex-row items-center w-[20rem] mobile:w-[16rem] h-20 bg-gray-400  rounded"
-      onMouseOver={!like ? () => setDisplayHeart(true) : () => { }}
-      onMouseLeave={!like ? () => setDisplayHeart(false) : () => { }}
-    >
-      <img src={image} alt={product} className="h-20 w-20 rounded-l" />
-      <div className="flex flex-col">
-        <div className="absolute flex flex-row ml-52 mt-3 mobile:ml-36">
-          {displayHeart ? (
-            <AiFillHeart
-              size={16}
-              onClick={() => setLike(!like)}
-              className="cursor-pointer"
-              color={`${like ? '#D0103F' : '#FFFFFF'}`}
-            />
-          ) : ''}
-        </div>
+    return (
+        <div
+            className="flex flex-row items-center sm:w-80 w-64  h-20 bg-gray-300  rounded"
+            onMouseOver={!like ? () => setDisplayHeart(true) : () => { }}
+            onMouseLeave={!like ? () => setDisplayHeart(false) : () => { }}
+        >
+            <img src={image} alt={product} className="h-20 w-20 rounded-l" />
+            <div className="flex flex-col">
+                <div className="absolute flex flex-row ml-36 sm:ml-52 mt-3">
+                    {displayHeart ? (
+                        <AiFillHeart
+                            size={16}
+                            onClick={() => setLike(!like)}
+                            className="cursor-pointer"
+                            color={`${like ? '#D0103F' : '#FFFFFF'}`}
+                        />
+                    ) : ''}
+                </div>
 
-        <div className="flex flex-col justify-center py-2 px-3  w-full cursor-pointer" onClick={openModal}>
-          <div className="flex flex-row justify-between">
-            <span className="font-montserrat font-semibold mb-1 mobile:text-sm">{product}</span>
-          </div>
-          <AssessmentsStars stars={stars} />
-          <span className="font-inter font-semibold text-sm mobile:text-xs mt-2 text-amber-900">{price}</span>
-        </div>
-      </div>
+                <div className="flex flex-col justify-center py-2 px-3  w-full cursor-pointer" onClick={openModal}>
+                    <div className="flex flex-row justify-between">
+                        <span className="font-montserrat font-semibold mb-1 text-sm sm:text-lg">{product}</span>
+                    </div>
+                    <AssessmentsStars stars={stars} />
+                    <span className="font-inter font-semibold text-xs sm:text-sm mt-2 text-amber-900">{price}</span>
+                </div>
+            </div>
 
-      <ModalContainer
-        title="Café simples"
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-      >
+            <ModalContainer
+                title="Café simples"
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+            >
 
-        <ModalService 
-          service="Café simples"
-          description="Café com açúcar saboroso preparado pela casa.
+                <ModalService
+                    service="Café simples"
+                    description="Café com açúcar saboroso preparado pela casa.
           Quantidade: 200 ml"
-          stars={3}
-          image={Coffee1}
-          price="R$ 2,00"
-          />
-        
-      </ModalContainer>
-    </div>
-  )
+                    stars={3}
+                    image={Coffee1}
+                    price="R$ 2,00"
+                />
+
+            </ModalContainer>
+        </div>
+    )
 }
