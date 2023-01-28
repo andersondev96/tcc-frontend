28644979c4e32b8f15e6b482200c1bed09799455
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-01-28 00:47:31 -0300

    Modified map screen

--- src/pages/client/Home.tsx
+++ src/pages/client/Home.tsx
@@ -110,104 +110,12 @@ export const Home: React.FC = () => {
         <div>
             <NavBar />
 
-            {/* <div className="flex flex-col mobile:flex-col-reverse">
-                                        <div className="flex flex-row">
-                                            <div className="flex flex-col justify-between gap-3 bg-blue-400 min-h-screen min-w-min py-24 px-24 mobile:hidden">
-                                                <div className="flex flex-col gap-4 justify-center">
-                                                    <select
-                                                        name="states"
-                                                        value={selectedUf}
-                                                        onChange={handleSelectedUf}
-                                                        placeholder="Estado"
-                                                    >
-                                                        <option value="0" disabled>
-                                                            Selecione uma opção
-                                                        </option>
-                                                        {ufs.map((uf) => (
-                                                            <option key={uf.id} value={uf.sigla}>
-                                                                {uf.nome}
-                                                            </option>
-                                                        ))}
-                                                    </select>
-
-                                                    <select
-                                                        name="cities"
-                                                        value={selectedCity}
-                                                        onChange={handleSelectedCity}
-                                                        placeholder="Cidade"
-                                                    >
-                                                        <option value="0" disabled>
-                                                            Selecione uma opção
-                                                        </option>
-                                                        {cities.map((city) => (
-                                                            <option key={city.id} value={city.nome}>
-                                                                {city.nome}
-                                                            </option>
-                                                        ))}
-                                                    </select>
-
-                                                    <select name="categories" placeholder="Categoria do negócio">
-                                                        <option value="default" disabled>
-                                                            Selecione uma opção
-                                                        </option>
-                                                        <option value="Cafeteria">Cafeteria</option>
-                                                        <option value="Sorveteria">Sorveteria</option>
-                                                    </select>
-
-                                                    <div className="flex flex-row justify-center mt-4">
-                                                        <button
-                                                            className="bg-indigo-500 h-10 w-28 rounded font-montserrat font-semibold text-white hover:brightness-90 transition-opacity"
-                                                            onClick={getLocation}
-                                                        >
-                                                            Pesquisar
-                                                        </button>
-                                                    </div>
-                                                </div>
-
-                                                {
-                                                    user ? (
-                                                        <div className="flex flex-row items-center gap-4">
-                                                            {
-                                                                user.avatar ? (
-                                                                    <img
-                                                                        src={user?.avatar}
-                                                                        alt={user?.name}
-                                                                        className="h-12 w-12 rounded-full border-x-slate-900 border-2 border-white"
-                                                                    />
-                                                                ) : <FaUserCircle size={36} />
-                                                            }
-                                                            <div className="flex flex-col">
-                                                                <span className="font-montserrat font-medium text-sm text-white">
-                                                                    {user?.name}
-                                                                </span>
-                                                                <span
-                                                                    className="font-montserrat font-light text-xs text-white text-right cursor-pointer hover:brightness-90 transition-shadow"
-                                                                    onClick={signOut}
-                                                                >
-                                                                    Sair
-                                                                </span>
-                                                            </div>
-                                                        </div>
-                                                    ) :
-                                                        <div className="flex flex-row items-center px-16">
-                                                            <button
-                                                                onClick={() => navigate("/login")}
-                                                                type="button"
-                                                                className="px-8 py-3 text-white bg-indigo-500 rounded focus:outline-none disabled:opacity-100 cursor-pointer hover:brightness-90"
-                                                            >
-                                                                Login
-                                                            </button>
-                                                        </div>
-                                                }
-                                                <div className="flex flex-col mt-8">
-                                                    <Range />
-                                                </div>
-                                            </div>
-                                            <div>
-                                                <Map lat={coords[0]} lng={coords[1]} />
-                                            </div>
-                                        </div>
-                                </div> */}
+            <div className="flex flex-col w-screen h-screen">
+                <div className="flex items-center justify-center py-2 bg-gray-600 bg-opacity-60">
+                    <span className="font-medium text-white">Selecione um ponto no mapa para ver algum empreendimento</span>
+                </div>
+                <Map lat={coords[0]} lng={coords[1]} />
+            </div>
         </div>
     );
 };
