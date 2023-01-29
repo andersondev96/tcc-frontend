fc21bc50741d4333547dff045104057b9aaca828
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-01-29 10:50:16 -0300

    Adjustments in clients page

--- src/pages/entrepreneur/ClientsEntrepreneur/index.tsx
+++ src/pages/entrepreneur/ClientsEntrepreneur/index.tsx
@@ -10,57 +10,57 @@ import { TableHeader } from "../../../components/Table/TableHead/TableHeader";
 import { TableRowHead } from "../../../components/Table/TableHead/TableRowHead";
 
 export const ClientsEntrepreneur: React.FC = () => {
-  return (
-    <div className="flex flex-row">
-      <SideBar />
-      <div className="flex flex-col w-full sm:ml-64">
-        <div className="flex flex-col items-center py-[2.5rem] mobile:py-[1.75rem]">
-          <h1 className="font-montserrat font-medium text-2xl">Clientes</h1>
-        </div>
-        <div className="flex flex-col px-12">
-          <Search />
-          <div className="mt-6">
-            <Table>
-              <TableHead>
-                <TableRowHead>
-                  <TableHeader>Nome</TableHeader>
-                  <TableHeader>E-mail</TableHeader>
-                  <TableHeader>Telefone</TableHeader>
-                </TableRowHead>
-              </TableHead>
-              <TableBody>
-                <TableRowBody>
-                  <TableData>João Pedro Xavier</TableData>
-                  <TableData>joao.xavier@gmail.com</TableData>
-                  <TableData>(99) 99999-9999</TableData>
-                </TableRowBody>
-                <TableRowBody>
-                  <TableData>João Pedro Xavier</TableData>
-                  <TableData>joao.xavier@gmail.com</TableData>
-                  <TableData>(99) 99999-9999</TableData>
-                </TableRowBody>
-                <TableRowBody>
-                  <TableData>João Pedro Xavier</TableData>
-                  <TableData>joao.xavier@gmail.com</TableData>
-                  <TableData>(99) 99999-9999</TableData>
-                </TableRowBody>
-                <TableRowBody>
-                  <TableData>João Pedro Xavier</TableData>
-                  <TableData>joao.xavier@gmail.com</TableData>
-                  <TableData>(99) 99999-9999</TableData>
-                </TableRowBody>
-                <TableRowBody>
-                  <TableData>João Pedro Xavier</TableData>
-                  <TableData>joao.xavier@gmail.com</TableData>
-                  <TableData>(99) 99999-9999</TableData>
-                </TableRowBody>
-              </TableBody>
-            </Table>
-          </div>
+    return (
+        <div className="flex flex-row">
+            <SideBar pageActive="clientes" />
+            <div className="flex flex-col w-full ml-12 sm:ml-64">
+                <div className="flex flex-col items-center py-7 sm:py-12">
+                    <h1 className="font-medium text-2xl">Clientes</h1>
+                </div>
+                <div className="flex flex-col px-12">
+                    <Search />
+                    <div className="mt-6">
+                        <Table>
+                            <TableHead>
+                                <TableRowHead>
+                                    <TableHeader>Nome</TableHeader>
+                                    <TableHeader>E-mail</TableHeader>
+                                    <TableHeader>Telefone</TableHeader>
+                                </TableRowHead>
+                            </TableHead>
+                            <TableBody>
+                                <TableRowBody>
+                                    <TableData>João Pedro Xavier</TableData>
+                                    <TableData>joao.xavier@gmail.com</TableData>
+                                    <TableData>(99) 99999-9999</TableData>
+                                </TableRowBody>
+                                <TableRowBody>
+                                    <TableData>João Pedro Xavier</TableData>
+                                    <TableData>joao.xavier@gmail.com</TableData>
+                                    <TableData>(99) 99999-9999</TableData>
+                                </TableRowBody>
+                                <TableRowBody>
+                                    <TableData>João Pedro Xavier</TableData>
+                                    <TableData>joao.xavier@gmail.com</TableData>
+                                    <TableData>(99) 99999-9999</TableData>
+                                </TableRowBody>
+                                <TableRowBody>
+                                    <TableData>João Pedro Xavier</TableData>
+                                    <TableData>joao.xavier@gmail.com</TableData>
+                                    <TableData>(99) 99999-9999</TableData>
+                                </TableRowBody>
+                                <TableRowBody>
+                                    <TableData>João Pedro Xavier</TableData>
+                                    <TableData>joao.xavier@gmail.com</TableData>
+                                    <TableData>(99) 99999-9999</TableData>
+                                </TableRowBody>
+                            </TableBody>
+                        </Table>
+                    </div>
 
-          <PaginationTable />
+                    <PaginationTable />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
