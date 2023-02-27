c503429f9724798a6e52b0a04fe0336a607b0b3d
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-02-26 21:54:52 -0300

    Show proposals

--- src/pages/entrepreneur/BudgetEntrepreneur/index.tsx
+++ src/pages/entrepreneur/BudgetEntrepreneur/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { format } from "date-fns";
+import React, { useCallback, useEffect, useState } from "react";
 import { AiOutlineEye } from "react-icons/ai";
 import { IoDocumentTextOutline } from "react-icons/io5";
 import { TbSend } from "react-icons/tb";
@@ -13,8 +14,38 @@ import { TableData } from "../../../components/Table/TableData";
 import { TableHead } from "../../../components/Table/TableHead";
 import { TableHeader } from "../../../components/Table/TableHead/TableHeader";
 import { TableRowHead } from "../../../components/Table/TableHead/TableRowHead";
+import api from "../../../services/api";
+
+interface Company {
+    id: string;
+}
+interface Proposals {
+    id: string;
+    objective: string;
+    time: string;
+    description: string;
+    status: string;
+    customer_id: string;
+    createdAt: string;
+}
 
 export const BudgetEntrepreneur: React.FC = () => {
+    const [company, setCompany] = useState<Company>({} as Company);
+    const [proposals, setProposals] = useState<Proposals[]>([]);
+
+    useEffect(() => {
+        api.get(`companies/me`).then((response) => setCompany(response.data));
+
+        api.get(`proposals/company/${company.id}`).then((response) => setProposals(response.data));
+    }, [company.id]);
+
+
+
+
+    const handleSearchBudgets = useCallback(() => {
+        //
+    }, []);
+
     return (
         <div className="flex flex-row">
             <SideBar pageActive="orcamentos" />
@@ -23,7 +54,7 @@ export const BudgetEntrepreneur: React.FC = () => {
                     <h1 className="font-montserrat font-medium text-2xl">Orçamentos</h1>
                 </div>
                 <div className="flex flex-col px-12">
-                    <Search />
+                    <Search onChange={handleSearchBudgets} />
                     <div className="mt-6">
                         <Table>
                             <TableHead>
@@ -37,86 +68,34 @@ export const BudgetEntrepreneur: React.FC = () => {
                                 </TableRowHead>
                             </TableHead>
                             <TableBody>
-                                <TableRowBody>
-                                    <TableData>04/07/2022</TableData>
-                                    <TableData>João Pedro Xavier</TableData>
-                                    <TableData>Cappuccino para festa</TableData>
-                                    <TableData>12/07/2022</TableData>
-                                    <TableData>Aguardando</TableData>
-                                    <TableData>
-                                        <div className="flex flex-row gap-2">
-                                            <Link to="/admin/budget/details">
-                                                <AiOutlineEye size={24} color="#547DE5" />
-                                            </Link>
-                                            <Link to="/admin/budget/create-proposal">
-                                                <IoDocumentTextOutline size={24} color="#1EBF1B" />
-                                            </Link>
-                                            <TbSend size={24} color="#EEB522" />
-                                        </div>
-                                    </TableData>
-                                </TableRowBody>
 
-                                <TableRowBody>
-                                    <TableData>04/07/2022</TableData>
-                                    <TableData>João Pedro Xavier</TableData>
-                                    <TableData>Cappuccino para festa</TableData>
-                                    <TableData>12/07/2022</TableData>
-                                    <TableData>Aguardando</TableData>
-                                    <TableData>
-                                        <div className="flex flex-row gap-2">
-                                            <Link to="/admin/budget/details">
-                                                <AiOutlineEye size={24} color="#547DE5" />
-                                            </Link>
-                                            <Link to="/admin/budget/create-proposal">
-                                                <IoDocumentTextOutline size={24} color="#1EBF1B" />
-                                            </Link>
-                                            <TbSend size={24} color="#EEB522" />
-                                        </div>
-                                    </TableData>
-                                </TableRowBody>
-
-                                <TableRowBody>
-                                    <TableData>04/07/2022</TableData>
-                                    <TableData>João Pedro Xavier</TableData>
-                                    <TableData>Cappuccino para festa</TableData>
-                                    <TableData>12/07/2022</TableData>
-                                    <TableData>Aguardando</TableData>
-                                    <TableData>
-                                        <div className="flex flex-row gap-2">
-                                            <Link to="/admin/budget/details">
-                                                <AiOutlineEye size={24} color="#547DE5" />
-                                            </Link>
-                                            <Link to="/admin/budget/create-proposal">
-                                                <IoDocumentTextOutline size={24} color="#1EBF1B" />
-                                            </Link>
-                                            <TbSend size={24} color="#EEB522" />
-                                        </div>
-                                    </TableData>
-                                </TableRowBody>
-
-                                <TableRowBody>
-                                    <TableData>04/07/2022</TableData>
-                                    <TableData>João Pedro Xavier</TableData>
-                                    <TableData>Cappuccino para festa</TableData>
-                                    <TableData>12/07/2022</TableData>
-                                    <TableData>Aguardando</TableData>
-                                    <TableData>
-                                        <div className="flex flex-row gap-2">
-                                            <Link to="/admin/budget/details">
-                                                <AiOutlineEye size={24} color="#547DE5" />
-                                            </Link>
-                                            <Link to="/admin/budget/create-proposal">
-                                                <IoDocumentTextOutline size={24} color="#1EBF1B" />
-                                            </Link>
-                                            <TbSend size={24} color="#EEB522" />
-                                        </div>
-                                    </TableData>
-                                </TableRowBody>
+                                {proposals.length > 0 ? proposals.map((proposal) => (
+                                    <TableRowBody>
+                                        <TableData>{format(new Date(proposal.createdAt), "dd/MM/yyyy")}</TableData>
+                                        <TableData>{proposal.customer_id}</TableData>
+                                        <TableData>{proposal.description}</TableData>
+                                        <TableData>{format(new Date(proposal.time), "dd/MM/yyyy")}</TableData>
+                                        <TableData>{proposal.status}</TableData>
+                                        <TableData>
+                                            <div className="flex flex-row gap-2">
+                                                <Link to="/admin/budget/details">
+                                                    <AiOutlineEye size={24} color="#547DE5" />
+                                                </Link>
+                                                <Link to="/admin/budget/create-proposal">
+                                                    <IoDocumentTextOutline size={24} color="#1EBF1B" />
+                                                </Link>
+                                                <TbSend size={24} color="#EEB522" />
+                                            </div>
+                                        </TableData>
+                                    </TableRowBody>
+                                )) : (
+                                    <p className="px-4">Nenhuma proposta encontrada</p>
+                                )}
                             </TableBody>
                         </Table>
                     </div>
 
-                    <PaginationTable />
+                    <PaginationTable results={proposals.length} />
                 </div>
             </div>
         </div>
