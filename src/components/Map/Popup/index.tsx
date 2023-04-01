2b430b4e4b8238722b53c0fe99c2cba15c97dfb6
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-04-01 14:41:18 -0300

    Favorite company in map

--- src/components/Map/Popup/index.tsx
+++ src/components/Map/Popup/index.tsx
@@ -4,8 +4,9 @@ import { MdOutlineChatBubbleOutline } from "react-icons/md";
 import { Popup as PopupContainer } from "react-leaflet";
 import { Link } from "react-router-dom";
 
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ModalCalculate } from "../../../pages/client/components/ModalCalculate";
+import api from '../../../services/api';
 import { ModalChat } from "../../ModalChat";
 import { ModalContainer } from "../../ModalContainer";
 import './styles.scss';
@@ -24,8 +25,27 @@ interface PopupProps {
     image: string;
 }
 export const Popup: React.FC<PopupProps> = ({ id, name, category, contact, image }) => {
+    const [companyFavorited, setCompanyFavorited] = useState(false);
     const [modalCalculeIsOpen, setModalCalculateIsOpen] = useState(false);
     const [modalChatIsOpen, setModalChatIsOpen] = useState(false);
+
+    const verifyCompanyIsFavorited = useCallback(() => {
+        api.get(`users/favorite/${id}`).then((response) => {
+            if (response.data.length > 0) {
+                setCompanyFavorited(true);
+            }
+        })
+    }, [setCompanyFavorited]);
+
+    const handleFavoriteCompany = useCallback(async () => {
+        if (!companyFavorited) {
+            await api.patch(`/companies/favorites/${id}`);
+            setCompanyFavorited(true);
+        } else {
+            await api.patch(`/companies/favorites/unfavorite/${id}`);
+            setCompanyFavorited(false);
+        }
+    }, [setCompanyFavorited, companyFavorited]);
 
     function openModalCalculate() {
         setModalCalculateIsOpen(true);
@@ -43,11 +63,15 @@ export const Popup: React.FC<PopupProps> = ({ id, name, category, contact, image
         setModalChatIsOpen(false);
     }
 
+    useEffect(() => {
+        verifyCompanyIsFavorited();
+    }, [verifyCompanyIsFavorited]);
+
     return (
         <PopupContainer closeButton={false}>
             <div className="leaflet-popup-container">
-                <div className="leaflet-popup-icon-like">
-                    <AiFillHeart className="icon-heart" size={18} />
+                <div className="leaflet-popup-icon-like" onClick={handleFavoriteCompany}>
+                    <AiFillHeart className={`icon-heart ${companyFavorited && 'favorited'}`} size={18} />
                 </div>
                 <Link to={`/business/${id}`}>
                     <div className="leaflet-popup-image-container">
@@ -71,7 +95,7 @@ export const Popup: React.FC<PopupProps> = ({ id, name, category, contact, image
                             </a>
                         )
                     }
-                    <a href={contact.email} target="_blank">
+                    <a href={`mailto:${contact.email}`} target="_blank">
                         <AiOutlineMail size={16} />
                     </a>
                     <MdOutlineChatBubbleOutline size={16} onClick={openModalChat} />
