b32bc4f97dea65fbf78f42edf76b05d39ae2d8ca
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-04-01 22:14:02 -0300

    Post Settings Entrepreneur

--- src/pages/entrepreneur/Settings/index.tsx
+++ src/pages/entrepreneur/Settings/index.tsx
@@ -1,8 +1,23 @@
-import { FormEvent, useCallback, useState } from "react";
+import { FormEvent, useCallback, useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import { SideBar } from "../../../components/Sidebar";
+import api from "../../../services/api";
 import { AssessmentsStars } from "../../client/components/AssessmentsStars";
 
+interface EntrepreneurSettingsData {
+    id: string;
+    entrepreneur_id: string;
+    service_name_color: string;
+    service_price_color: string;
+    card_color: string;
+    highlight_services_quantity: number;
+    online_budget: boolean;
+    online_chat: boolean;
+    email_notification: boolean;
+}
+
 export const Settings: React.FC = () => {
+    const [entrepreneurSettings, setEntrepreneurSettings] = useState<EntrepreneurSettingsData>({} as EntrepreneurSettingsData);
     const [colorNameService, setColorNameService] = useState("");
     const [colorPriceService, setColorPriceService] = useState("");
     const [cardColor, setCardColor] = useState("");
@@ -11,21 +26,49 @@ export const Settings: React.FC = () => {
     const [onlineChatActive, setOnlineChatActive] = useState(false);
     const [emailNotification, setEmailNotification] = useState(false);
 
+    useEffect(() => {
+        api.get(`/entrepreneurs`).then((response) => {
+            setEntrepreneurSettings(response.data);
+        })
+    }, []);
 
-    const handleSubmit = useCallback((event: FormEvent) => {
+
+    const handleSubmit = useCallback(async (event: FormEvent) => {
         event.preventDefault();
 
         const data = {
-            colorNameService,
-            colorPriceService,
-            cardColor,
-            quantityServices,
-            budgetActive,
-            onlineChatActive,
-            emailNotification
+            service_name_color: colorNameService,
+            service_price_color: colorPriceService,
+            card_color: cardColor,
+            highlight_services_quantity: quantityServices,
+            online_budget: budgetActive,
+            online_chat: onlineChatActive,
+            email_notification: emailNotification
         }
-        console.log(data);
-    }, [colorNameService, colorPriceService, cardColor, quantityServices, budgetActive, onlineChatActive, emailNotification]);
+
+        const settings = await api.put(`/entrepreneurs/${entrepreneurSettings.entrepreneur_id}/settings`, data);
+
+        console.log(settings);
+
+        if (settings.status === 201) {
+            toast.success("Preferências alteradas com sucesso!");
+        } else {
+            toast.error("Erro ao alterar as preferências!");
+        }
+
+
+    }, [
+        colorNameService,
+        colorPriceService,
+        cardColor,
+        quantityServices,
+        budgetActive,
+        onlineChatActive,
+        emailNotification,
+        entrepreneurSettings.entrepreneur_id,
+        toast
+    ]);
+
     return (
         <div className="flex flex-row">
             <SideBar pageActive="configuracoes" />
@@ -51,7 +94,7 @@ export const Settings: React.FC = () => {
                                     </label>
                                     <input
                                         type="color"
-                                        value={colorNameService}
+                                        value={entrepreneurSettings.service_name_color}
                                         name="colorNameService"
                                         onChange={e => setColorNameService(e.target.value)}
                                         className="appearance-none w-20 h-8 border-none rounded bg-transparent cursor-pointer"
@@ -67,7 +110,7 @@ export const Settings: React.FC = () => {
                                     </label>
                                     <input
                                         type="color"
-                                        value={colorPriceService}
+                                        value={entrepreneurSettings.service_price_color}
                                         name="colorPriceService"
                                         onChange={e => setColorPriceService(e.target.value)}
                                         className="appearance-none w-20 h-8 border-none rounded bg-transparent cursor-pointer"
@@ -83,7 +126,7 @@ export const Settings: React.FC = () => {
                                     </label>
                                     <input
                                         type="color"
-                                        value={cardColor}
+                                        value={entrepreneurSettings.card_color}
                                         name="cardColor"
                                         onChange={e => setCardColor(e.target.value)}
                                         className="appearance-none w-20 h-8 border-none rounded bg-transparent cursor-pointer"
@@ -95,12 +138,12 @@ export const Settings: React.FC = () => {
                             <span className=" font-regular text-sm">
                                 Pré-visualização:
                             </span>
-                            <div className={`"flex flex-col justify-center px-16  w-56 h-16 bg-[${cardColor}] rounded"`}>
-                                <span className={`font-semibold text-sm text-[${colorNameService}]`}>
+                            <div className={`"flex flex-col justify-center px-16  w-56 h-16 bg-[${entrepreneurSettings.card_color}] rounded"`}>
+                                <span className={`font-semibold text-sm text-[${entrepreneurSettings.service_name_color}]`}>
                                     Serviço
                                 </span>
                                 <AssessmentsStars stars={5} />
-                                <strong className={`font-inter font-semibold text-xs text-[${colorPriceService}]`}>
+                                <strong className={`font-inter font-semibold text-xs text-[${entrepreneurSettings.service_price_color}]`}>
                                     R$ 4,50
                                 </strong>
                             </div>
@@ -116,7 +159,7 @@ export const Settings: React.FC = () => {
                                 type="number"
                                 min={1}
                                 max={5}
-                                defaultValue={3}
+                                defaultValue={entrepreneurSettings.highlight_services_quantity}
                                 onChange={e => setQuantityServices(Number(e.target.value))}
                                 className="w-16 h-10 p-2 rounded bg-gray-200 border-none font-medium text-sm text-gray-800"
                             />
@@ -129,6 +172,7 @@ export const Settings: React.FC = () => {
                                 <input
                                     type="checkbox"
                                     id="budget"
+                                    defaultChecked={entrepreneurSettings.online_budget}
                                     onChange={() => setBudgetActive(!emailNotification)}
                                     className="rounded border-2 border-gray-900"
                                 />
@@ -142,6 +186,7 @@ export const Settings: React.FC = () => {
                             <div className="flex flex-row items-center gap-2">
                                 <input
                                     type="checkbox"
+                                    defaultChecked={entrepreneurSettings.online_chat}
                                     id="chat"
                                     onChange={() => setOnlineChatActive(!onlineChatActive)}
                                     className="rounded border-2 border-gray-900"
@@ -156,6 +201,7 @@ export const Settings: React.FC = () => {
                             <div className="flex flex-row items-center gap-2">
                                 <input
                                     type="checkbox"
+                                    defaultChecked={entrepreneurSettings.email_notification}
                                     id="email"
                                     onChange={() => setEmailNotification(!emailNotification)}
                                     className="rounded border-2 border-gray-900"
