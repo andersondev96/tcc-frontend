94bbbd97c765a28e85c611cf73e176974a69e8b5
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-04-09 21:59:18 -0300

    Connect chat

--- src/components/ModalChat/components/WelcomeChat.tsx
+++ src/components/ModalChat/components/WelcomeChat.tsx
@@ -1,19 +1,23 @@
 import { ChatIcon } from '@heroicons/react/solid';
 import { FormHandles } from "@unform/core";
 import { Form } from "@unform/web";
-import { FormEvent, useCallback, useRef } from "react";
+import { useRef } from "react";
 import { useAuth } from '../../../contexts/AuthContext';
 import { Input } from "../../Form/Input";
 
-export const WelcomeChat: React.FC = () => {
+interface FormData {
+    name: string;
+    email: string;
+    telephone: string;
+}
+interface Props {
+    handleSubmit: (data: FormData) => void;
+}
+
+export const WelcomeChat: React.FC<Props> = ({ handleSubmit }) => {
     const formRef = useRef<FormHandles>(null);
 
     const { user } = useAuth();
-
-    const handleSubmit = useCallback((event: FormEvent) => {
-        event.preventDefault();
-        console.log(event);
-    }, []);
 
     return (
         <div className="flex flex-col items-center mt-12 justify-center">
@@ -24,19 +28,19 @@ export const WelcomeChat: React.FC = () => {
                 initialData={user}
             >
                 <div>
-                    <Input name="name" label="Nome" />
+                    <Input id="name" name="name" label="Nome" />
                 </div>
 
                 <div>
-                    <Input name="email" label="E-mail" type="email" />
+                    <Input id="email" name="email" label="E-mail" type="email" />
                 </div>
 
                 <div>
-                    <Input name="telephone" label="Telefone" type="phone" />
+                    <Input id="telephone" name="telephone" label="Telefone" type="phone" />
                 </div>
 
                 <div>
-                    <button type="button" className="flex flex-row items-center gap-14 w-full h-12 rounded p-2 bg-blue-500 hover:brightness-90 duration-300 transition-all">
+                    <button type="submit" className="flex flex-row items-center gap-14 w-full h-12 rounded p-2 bg-blue-500 hover:brightness-90 duration-300 transition-all">
                         <ChatIcon className="w-8 text-white" />
                         <span className="font-semibold text-lg text-white">Iniciar o chat</span>
                     </button>
