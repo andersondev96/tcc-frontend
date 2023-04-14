28717edb4c3dc828aa6f09811f51e55a4c36590a
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-04-14 20:27:59 -0300

    Update image avatar in navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -157,15 +157,25 @@ export const NavBar: React.FC<NavBarProps> = ({ pageCurrent }) => {
                                                     <span className="sr-only">Abrir menu do usuário</span>
 
                                                     <div className="flex items-center">
-                                                        {!googleUser && user.avatar ? (
+                                                        {!googleUser && user.avatar && (
                                                             <div className="shrink-0">
                                                                 <img src={`http://localhost:3333/avatar/${user.avatar}`} alt="Avatar" className="rounded-full h-8 sm:h-10 w-8 sm:w-10" />
                                                             </div>
-                                                        ) : (
+                                                        )}
+
+                                                        {googleUser && !user.avatar && (
                                                             <div className="shrink-0">
-                                                                <img src={googleUser?.avatar} alt="Avatar" className="rounded-full h-8 sm:h-10 w-8 sm:w-10" />
+                                                                <img src={googleUser && googleUser?.avatar} alt="Avatar" className="rounded-full h-8 sm:h-10 w-8 sm:w-10" />
                                                             </div>
                                                         )}
+
+                                                        {
+                                                            user && !user.avatar && (
+                                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#9ca3af" className="w-8 h-8 sm:h-10 w-8 sm:w-10">
+                                                                    <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clipRule="evenodd" />
+                                                                </svg>
+                                                            )
+                                                        }
 
                                                     </div>
                                                 </Menu.Button>
