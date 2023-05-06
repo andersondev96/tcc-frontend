1ebd22ff738cb683bf43b49439ca7f799b53d150
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-05-06 19:13:33 -0300

    Delete unessessary files

--- src/hooks/useAuthWithGoogle.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { AuthContextWithGoogle } from "../contexts/AuthContextWithGoogle";
-
-export function useAuthWithGoogle() {
-    const value = useContext(AuthContextWithGoogle);
-
-    return value;
-}
